b694cfc07e024c9ee0633b3fec5ddbeb18a79a1e
Michał Pielak <michal.pielak81@gmail.com>
2024-07-06 13:27:06 +0200

    Poprawa dla builda

--- src/app/produkt/[productSlug]/layout.tsx
+++ src/app/produkt/[productSlug]/layout.tsx
@@ -1,13 +1,10 @@
-"use client";
-import BreadcrumbsForProductPage from "@/components/product/ProductBreadcrumbs";
 import { ReactNode } from "react";
 
 interface ProductLayoutProps {
 	children: ReactNode;
-	pageProps: any;
 }
 
-export const ProductLayout = ({ children }: ProductLayoutProps) => {
+const ProductLayout = ({ children }: ProductLayoutProps) => {
 	return (
 		<div className="flex w-full max-w-screen-xl flex-wrap items-start justify-between">
 			{children}
