2f9ab1f4aead811247a4883aa6d1af8f55eb8071
Michał Pielak <michal.pielak81@gmail.com>
2024-08-22 16:00:38 +0200

    Zmiana linku z /cart na /koszyk

--- src/components/cart/CartDetailsButton.tsx
+++ src/components/cart/CartDetailsButton.tsx
@@ -1,6 +1,6 @@
 export function CartDetailsButton() {
 	return (
-		<a className="w-full" href="/cart">
+		<a className="w-full" href="/koszyk">
 			<button className="mx-auto mt-4 w-full rounded-lg border bg-slate-950 py-2 text-white shadow transition-colors hover:bg-slate-800 dark:text-white">
 				Idź do koszyka
 			</button>
--- src/components/cart/ShopingCartLink.tsx
+++ src/components/cart/ShopingCartLink.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export const ShopingCartLink = () => {
 	return (
-		<Link href="/cart" className="group m-2 flex h-full items-center p-2">
+		<Link href="/koszyk" className="group m-2 flex h-full items-center p-2">
 			<ShoppingCart className="ml-4 h-6 w-6 flex-shrink" aria-hidden="true" />
 		</Link>
 	);
