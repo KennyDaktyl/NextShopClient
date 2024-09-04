104ea99b3549646d7357491a33cd81d40d519fc0
Michał Pielak <michal.pielak81@gmail.com>
2024-09-04 20:40:34 +0200

    fix no-store

--- src/components/account/ClientProfilePage.tsx
+++ src/components/account/ClientProfilePage.tsx
@@ -26,10 +26,10 @@ export default function ClientProfilePage({ user, token }: ClientProfilePageProp
 		resolver: zodResolver(basicUserDataSchema),
 		defaultValues: {
 			username: user.username,
-			first_name: user.first_name,
-			last_name: user.last_name,
+			first_name: user.first_name ?? "",
+			last_name: user.last_name ?? "",
 			email: user.email,
-			mobile: user.profile.mobile || "",
+			mobile: user.profile?.mobile ?? "",
 		},
 	});
 
@@ -37,11 +37,11 @@ export default function ClientProfilePage({ user, token }: ClientProfilePageProp
 	const addressUserDataFormMethods = useForm({
 		resolver: zodResolver(addressUserDataSchema),
 		defaultValues: {
-			street: user.profile.street || "",
-			house_number: user.profile.house_number || "",
-			local_number: user.profile.local_number || "",
-			postal_code: user.profile.postal_code || "",
-			city: user.profile.city || "",
+			street: user.profile?.street ?? "",
+			house_number: user.profile?.house_number ?? "",
+			local_number: user.profile?.local_number ?? "",
+			postal_code: user.profile?.postal_code ?? "",
+			city: user.profile?.city ?? "",
 		},
 	});
 
@@ -49,14 +49,14 @@ export default function ClientProfilePage({ user, token }: ClientProfilePageProp
 	const invoiceUserDataFormMethods = useForm({
 		resolver: zodResolver(invoiceUserDataSchema),
 		defaultValues: {
-			company: user.profile.company || "",
-			company_payer: user.profile.company_payer || "",
-			nip: user.profile.nip || "",
-			invoice_street: user.profile.invoice_street || "",
-			invoice_house_number: user.profile.invoice_house_number || "",
-			invoice_local_number: user.profile.invoice_local_number || "",
-			invoice_city: user.profile.invoice_city || "",
-			invoice_postal_code: user.profile.invoice_postal_code || "",
+			company: user.profile?.company ?? "",
+			company_payer: user.profile?.company_payer ?? "",
+			nip: user.profile?.nip ?? "",
+			invoice_street: user.profile?.invoice_street ?? "",
+			invoice_house_number: user.profile?.invoice_house_number ?? "",
+			invoice_local_number: user.profile?.invoice_local_number ?? "",
+			invoice_city: user.profile?.invoice_city ?? "",
+			invoice_postal_code: user.profile?.invoice_postal_code ?? "",
 		},
 	});
 
--- src/components/category/SubcategoryItem.tsx
+++ src/components/category/SubcategoryItem.tsx
@@ -10,15 +10,15 @@ const SubcategoryItem = ({ item }: SubcategoryItemProps) => {
 	return (
 		<Link
 			href={item.full_path}
-			className="subcategory-list-item flex h-[410px] cursor-pointer flex-col items-center justify-start rounded-md border shadow-md transition duration-300 ease-in-out hover:border-gray-300 hover:shadow-lg md:h-[580px]"
+			className="subcategory-list-item flex h-[450px] cursor-pointer flex-col items-center justify-start rounded-md border shadow-md transition duration-300 ease-in-out hover:border-gray-300 hover:shadow-lg md:h-[520px]"
 		>
-			<div className="flex h-[360px] w-full flex-wrap justify-center p-2">
+			<div className="flex w-full flex-wrap justify-center p-2">
 				<h2 className="mt-2 h-[40px] text-center text-sm font-bold md:text-xl">{item.name}</h2>
-				<p className="mt-4 h-[200px] text-center text-xs md:text-sm">{item.description}</p>
+				<p className="mt-2 h-[100px] text-center text-xs md:text-sm">{item.description}</p>
 			</div>
 			{item.image && (
-				<div className="xs:h-[125px] mt-4 flex h-[125px] w-full items-center justify-center sm:h-[270px] sm:w-[270px]">
-					<div className="relative flex h-[125px] w-[125px] items-center justify-center overflow-hidden sm:h-[270px] sm:w-[270px]">
+				<div className="mb-4 mt-auto flex w-full items-center justify-center">
+					<div className="relative flex h-[125px] w-[125px] items-center justify-center overflow-hidden sm:h-[200px] sm:w-[200px]">
 						<Image
 							src={item.image.url || ""}
 							alt={item.image.alt || item.name}
@@ -26,7 +26,7 @@ const SubcategoryItem = ({ item }: SubcategoryItemProps) => {
 							loading="lazy"
 							className="rounded-md object-cover"
 							fill
-							sizes="(max-width: 768px) 100vw, 270px"
+							sizes="(max-width: 768px) 100vw, 200px"
 						/>
 					</div>
 				</div>
