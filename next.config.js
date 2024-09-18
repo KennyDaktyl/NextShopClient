7fa1ec53583359a91b6fd832af97ad3ee3696563
Michał Pielak <michal.pielak81@gmail.com>
2024-09-18 17:25:27 +0200

    CSP

--- next.config.js
+++ next.config.js
@@ -22,6 +22,29 @@ const nextConfig = {
 	env: {
 		API_URL: process.env.API_URL,
 	},
+	async headers() {
+		return [
+			{
+				source: "/(.*)",
+				headers: [
+					{
+						key: "Content-Security-Policy",
+						value: `
+				  default-src 'self'; 
+				  script-src 'self' 'unsafe-inline' 'unsafe-eval' https://apis.google.com https://geowidget.inpost.pl; 
+				  style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://geowidget.inpost.pl; 
+				  img-src 'self' data: https://geowidget.inpost.pl; 
+				  font-src 'self' https://fonts.gstatic.com; 
+				  connect-src 'self' https://serwiswrybnej.pl https://serwiswrybnej.pl https://new-serwiswrybnej-api.resto-app.pl http://127.0.0.1:8000 https://geowidget.inpost.pl; 
+				  frame-src 'self' https://geowidget.inpost.pl https://geowidget-app.inpost.pl;
+				`
+							.replace(/\s{2,}/g, " ")
+							.trim(),
+					},
+				],
+			},
+		];
+	},
 };
 
 module.exports = nextConfig;
