cb64dd6ec1ed2dd20bfb2922d6b58e71b2e3b362
Michał Pielak <michal.pielak81@gmail.com>
2024-10-24 10:28:40 +0200

    Dodanie suspense

--- src/components/product/ProductDetails.tsx
+++ src/components/product/ProductDetails.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
 import { DEFAULT_IMAGE_URL, formatMoney, trackViewItem } from "@/utils";
 import { BackLinkProps, ProductDetails, Variant } from "@/app/types";
@@ -287,80 +287,84 @@ export const ProductDetailsComponent = ({
 					<DescriptionComponent title="Opis produktu" description={product.description} />
 				)}
 			</div>
-			<Tabs defaultValue="details" className="w-full">
-				<TabsList className="grid w-full grid-cols-3 items-center justify-center rounded-md bg-muted p-1 text-muted-foreground">
-					<TabsTrigger
-						value="details"
-						className="h-14 rounded-md data-[state=active]:rounded-md data-[state=active]:bg-white data-[state=active]:text-black"
-					>
-						Opis produktu
-					</TabsTrigger>
-					<TabsTrigger
-						value="specyfication"
-						className="h-14 rounded-md data-[state=active]:rounded-md data-[state=active]:bg-white data-[state=active]:text-black"
-					>
-						Specyfikacja
-					</TabsTrigger>
-					<TabsTrigger
-						value="reviews"
-						className="h-14 rounded-md data-[state=active]:rounded-md data-[state=active]:bg-white data-[state=active]:text-black"
-					>
-						Opinie
-					</TabsTrigger>
-				</TabsList>
-				<TabsContent value="details">
-					{product.seo_text && (
-						<DescriptionComponent title="Szczegóły produktu" description={product.seo_text} />
-					)}
-				</TabsContent>
-				<TabsContent value="specyfication">
-					<p className="mb-3 mt-3 px-2">Brak specyfikacji</p>
-				</TabsContent>
-				<TabsContent value="reviews" className="p-2">
-					<h2 className="mb-4 text-2xl font-bold">Opinie o produkcie</h2>
-					<Button onClick={toggleReviewModal} className="mb-3 mt-1">
-						Dodaj opinię
-					</Button>
-					<Dialog open={isModalReviewOpen} onOpenChange={setIsModalReviewOpen}>
-						<DialogContent>
-							<DialogHeader>
-								<DialogTitle>Dodaj opinię o produkcie</DialogTitle>
-								<DialogClose className="absolute right-2 top-2"></DialogClose>
-							</DialogHeader>
-							<ReviewForm
-								product_id={product.id}
-								ratings={product.average_rating}
-								reviewsCount={product.review_count}
-								onSuccess={() => setIsModalReviewOpen(false)}
-							/>
-						</DialogContent>
-					</Dialog>
-					{product.review_count === 0 ? (
-						<p className="mb-3 mt-3 px-2">Brak opinii</p>
-					) : (
-						<div className="space-y-4">
-							{product.reviews.map((review) => (
-								<div key={review.id} className="rounded-lg border bg-gray-50 p-4">
-									<div className="flex items-center">
-										<span className="mr-2 text-sm font-semibold text-gray-500">{review.name}</span>
-										<span className="mr-2 text-sm text-gray-500">
-											{format(new Date(review.created_at), "dd MMMM yyyy", { locale: pl })}
-										</span>
-										<StarRatings
-											rating={review.rating}
-											starRatedColor="gold"
-											numberOfStars={5}
-											starDimension="20px"
-											starSpacing="5px"
-										/>
+			<Suspense>
+				<Tabs defaultValue="details" className="w-full">
+					<TabsList className="grid w-full grid-cols-3 items-center justify-center rounded-md bg-muted p-1 text-muted-foreground">
+						<TabsTrigger
+							value="details"
+							className="h-14 rounded-md data-[state=active]:rounded-md data-[state=active]:bg-white data-[state=active]:text-black"
+						>
+							Opis produktu
+						</TabsTrigger>
+						<TabsTrigger
+							value="specyfication"
+							className="h-14 rounded-md data-[state=active]:rounded-md data-[state=active]:bg-white data-[state=active]:text-black"
+						>
+							Specyfikacja
+						</TabsTrigger>
+						<TabsTrigger
+							value="reviews"
+							className="h-14 rounded-md data-[state=active]:rounded-md data-[state=active]:bg-white data-[state=active]:text-black"
+						>
+							Opinie
+						</TabsTrigger>
+					</TabsList>
+					<TabsContent value="details">
+						{product.seo_text && (
+							<DescriptionComponent title="Szczegóły produktu" description={product.seo_text} />
+						)}
+					</TabsContent>
+					<TabsContent value="specyfication">
+						<p className="mb-3 mt-3 px-2">Brak specyfikacji</p>
+					</TabsContent>
+					<TabsContent value="reviews" className="p-2">
+						<h2 className="mb-4 text-2xl font-bold">Opinie o produkcie</h2>
+						<Button onClick={toggleReviewModal} className="mb-3 mt-1">
+							Dodaj opinię
+						</Button>
+						<Dialog open={isModalReviewOpen} onOpenChange={setIsModalReviewOpen}>
+							<DialogContent>
+								<DialogHeader>
+									<DialogTitle>Dodaj opinię o produkcie</DialogTitle>
+									<DialogClose className="absolute right-2 top-2"></DialogClose>
+								</DialogHeader>
+								<ReviewForm
+									product_id={product.id}
+									ratings={product.average_rating}
+									reviewsCount={product.review_count}
+									onSuccess={() => setIsModalReviewOpen(false)}
+								/>
+							</DialogContent>
+						</Dialog>
+						{product.review_count === 0 ? (
+							<p className="mb-3 mt-3 px-2">Brak opinii</p>
+						) : (
+							<div className="space-y-4">
+								{product.reviews.map((review) => (
+									<div key={review.id} className="rounded-lg border bg-gray-50 p-4">
+										<div className="flex items-center">
+											<span className="mr-2 text-sm font-semibold text-gray-500">
+												{review.name}
+											</span>
+											<span className="mr-2 text-sm text-gray-500">
+												{format(new Date(review.created_at), "dd MMMM yyyy", { locale: pl })}
+											</span>
+											<StarRatings
+												rating={review.rating}
+												starRatedColor="gold"
+												numberOfStars={5}
+												starDimension="20px"
+												starSpacing="5px"
+											/>
+										</div>
+										<p className="mt-2 text-gray-700">{review.message}</p>
 									</div>
-									<p className="mt-2 text-gray-700">{review.message}</p>
-								</div>
-							))}
-						</div>
-					)}
-				</TabsContent>
-			</Tabs>
+								))}
+							</div>
+						)}
+					</TabsContent>
+				</Tabs>
+			</Suspense>
 		</div>
 	);
 };
